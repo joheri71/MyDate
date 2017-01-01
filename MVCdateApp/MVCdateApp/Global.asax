7bc81e69d5fa9b30b42c0548552590cce66218fc
Daniel Toll <daniel_toll@hotmail.com>
2017-01-01 22:26:12 +0100

    Startup.

---
+++ MVCdateApp/MVCdateApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCdateApp.MvcApplication" Language="C#" %>
---
+++ MVCdateApp/MVCdateApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCdateApp.Startup))]
+namespace MVCdateApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
