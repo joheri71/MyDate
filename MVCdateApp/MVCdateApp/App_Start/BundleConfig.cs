d1918d96ee5926a14bcdeadf0a7b2af44507ff55
Daniel Toll <daniel_toll@hotmail.com>
2017-01-02 07:18:49 +0100

    ändrade login och reg knapparna på hpme/index, la la till glyphicons

--- MVCdateApp/MVCdateApp/App_Start/BundleConfig.cs
+++ MVCdateApp/MVCdateApp/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace MVCdateApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = false;
+
         }
     }
 }
